065cc50a9c16dfc4ec04f7fed0e0fc2805a45d49
Agnieszka Marszałek <agnieszka.marszalek07@gmail.com>
2018-11-29 20:06:10 +0100

    deleted PatientService and PatientServiceImpl

--- laboratory/src/main/java/com/amarszalek/laboratory/controller/PatientController.java
+++ laboratory/src/main/java/com/amarszalek/laboratory/controller/PatientController.java
@@ -2,8 +2,7 @@ package com.amarszalek.laboratory.controller;
 
 import com.amarszalek.laboratory.exception.PatientNotFoundException;
 import com.amarszalek.laboratory.model.Patient;
-import com.amarszalek.laboratory.service.PatientService;
-import com.amarszalek.laboratory.service.PatientServiceImpl;
+import com.amarszalek.laboratory.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,26 +10,28 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/patient")
+@RequestMapping("/patients")
 @CrossOrigin(origins = "*")
 public class PatientController {
 
-
-    private PatientService patientService = new PatientServiceImpl();
+    @Autowired
+    private PatientRepository patientRepository;
 
     @GetMapping()
     public List<Patient> findAll(){
-        return patientService.findAll();
+        return patientRepository.findAll();
     }
 
     @PostMapping()
     public Patient createPatient(@RequestBody Patient patient){
-        return patientService.createPatient(patient);
+        return patientRepository.save(patient);
     }
 
     @GetMapping()
     @RequestMapping(params = "pesel")
-    public Patient findByPesel(@RequestParam String pesel){
-        return patientService.findByPesel(pesel);
+    public Patient findByPesel(@RequestParam String pesel) {
+        Optional<Patient> patient = patientRepository.findByPesel(pesel);
+        return patient.orElseThrow(() -> new PatientNotFoundException("Patient not found by pesel: " + pesel));
+
     }
 }
--- laboratory/src/main/java/com/amarszalek/laboratory/repository/PatientRepository.java
+++ laboratory/src/main/java/com/amarszalek/laboratory/repository/PatientRepository.java
@@ -4,6 +4,10 @@ package com.amarszalek.laboratory.repository;
 import com.amarszalek.laboratory.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    Patient findByPesel(String pesel);
+    Optional<Patient> findByPesel(String pesel);
+
 }
